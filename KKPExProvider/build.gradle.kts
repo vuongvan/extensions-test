6dd6ceb6d22d0fc9e191fd2bb0cf6e9fcb023124
Fei Yu <maivanvuong@gmail.com>
2026-06-19 22:47:24 +0700

    Create build.gradle.kts

---
+++ KKPExProvider/build.gradle.kts
@@ -0,0 +1,39 @@
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+}
+
+// Use an integer for version numbers
+version = 19
+
+cloudstream {
+    // All of these properties are optional, you can safely remove any of them.
+
+    description = "Movies"
+    authors = listOf("VM", "Gemini")
+
+    /**
+    * Status int as one of the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta-only
+    **/
+    status = 1 // Will be 3 if unspecified
+
+    tvTypes = listOf("Movie")
+
+    requiresResources = true
+    language = "vi"
+
+    // Random CC logo I found
+    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
