9dbdd987f8fd01487265caa4b63707635bf78c67
Fei Yu <maivanvuong@gmail.com>
2026-06-19 23:22:09 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream.gradle:gradle:81b1d424d")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
